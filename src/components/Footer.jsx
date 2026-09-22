42276a6eb81833dfc4597711d0791ac4fcd5c3a9
Ricardo Salmerón <ricardosalmeronps@gmail.com>
2026-09-22 20:25:48 -0300

    [Style] Centralizando os elementos do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,10 @@ const Footer = () => {
   return (
     <>
       <footer className="w-full min-h-2 flex items-center justify-between py-4 px-8 box-border bg-red-900 text-white transition-colors duration-0.3s z-1000">
-      <div className="footer-logo">
+      <div className=" flex-1 flex justify-start footer-logo">
         <h2 className="text-xl">GourmetOn</h2>
       </div>
-
-      <nav className="items-center flex space-x-4">
+      <nav className="flex-1 flex justify-center space-x-4">
         <a href="mailto:gorumetoncontato@gourmeton.com" className="text-white hover:underline">
           Email de Contato
         </a>
@@ -16,14 +15,15 @@ const Footer = () => {
         <a href="https://www.x.com/gourmeton" target="_blank" rel="noopener noreferrer" className="text-white hover:underline">
           X (Twitter)
         </a>
-        <a href="" className="text-white hover:underline">
+        <a href="" className=" text-white hover:underline">
           Termos de Uso
         </a>
       </nav>
-
-      <p className="footer-copy text-white text-sm">
-        © 2026 GourmetOn. Todos os direitos reservados.
-      </p>
+      <div className="flex-1 flex justify-end">
+        <p className="footer-copy text-white text-sm">
+          © 2026 GourmetOn. Todos os direitos reservados.
+        </p>
+      </div>
     </footer>
     </>
   )
